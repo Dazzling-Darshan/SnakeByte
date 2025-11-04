03f6dbcc94fedf6eaca2192db8bbf395c2f829c7
Mayank Jayswal <jayswalmayank05@gmail.com>
2025-11-04 15:09:13 +0530

    main updated

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() {
     cout << "Starting Cross-Platform Snake Game..." << endl;
     cout << "Make sure your terminal supports emojis!" << endl;
-    cout << "Starting in few seconds..." << endl;
+    cout << "Starting in a few seconds..." << endl;
     
     this_thread::sleep_for(chrono::seconds(2));
     
